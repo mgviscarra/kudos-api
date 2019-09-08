e7b54b59ef2ed20c4505cee47ef6a973b91a5876
Mauricio Viscarra <mgviscarra@gmail.com>
2019-09-08 01:49:39 -0400

    Adding base project for kudos-api

---
+++ src/main/java/com/mgvr/kudos/api/KudosApiApplication.java
@@ -0,0 +1,13 @@
+package com.mgvr.kudos.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KudosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KudosApiApplication.class, args);
+	}
+
+}
